4afdfa741177a25b25e14a30f60ac4282244f892
Tatyana Emelyanova <isstic.tt@gmail.com>
2018-05-04 13:58:14 +0300

    comments has been added

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -56,14 +56,14 @@ BOOST_AUTO_TEST_CASE(empty_bulk)
     test_prosess("2", in_data, out_data);
 }
 
-BOOST_AUTO_TEST_CASE(static_bulk)
+BOOST_AUTO_TEST_CASE(static_bulk_1)
 {
   
     std::string  in_data = 
     "cmd1\n"
     "cmd2\n"
     "cmd3\n"
-    "\n";
+    "\n";           // данные закончились/resync
 
     std::string out_data =
     "bulk: cmd1, cmd2\n"
@@ -71,6 +71,24 @@ BOOST_AUTO_TEST_CASE(static_bulk)
 
     test_prosess("2", in_data, out_data);
 }
+
+
+BOOST_AUTO_TEST_CASE(static_bulk_2)
+{
+  
+    std::string  in_data = 
+    "cmd1\n"
+    "cmd2\n"
+    "cmd3\n"
+    "cmd4\n";
+
+    std::string out_data =
+    "bulk: cmd1, cmd2\n"
+    "bulk: cmd3, cmd4\n";
+
+    test_prosess("2", in_data, out_data);
+}
+
 
 BOOST_AUTO_TEST_CASE(dinamic_bulk)
 { 
@@ -85,10 +103,10 @@ BOOST_AUTO_TEST_CASE(dinamic_bulk)
     "}\n"
     "cmd7\n"
     "cmd8\n"
-    "{\n"
-    "cmd9\n"
-    "cmd10\n"
-    "\n";
+    "{\n"           // new dinamic bulk
+    "cmd9\n"        // wrong data
+    "cmd10\n";      // wrong data
+    "\n";           // данные закончились/resync
 
     std::string out_data =
     "bulk: cmd1, cmd2\n"
@@ -123,12 +141,12 @@ BOOST_AUTO_TEST_CASE(dinamic_enclosed_bulk)
 BOOST_AUTO_TEST_CASE(invalid_bulk_1)
 {
     std::string  in_data = 
-    "cmd1\n"
-    "}\n"
-    "cmd3\n"
-    "cmd4\n"
-    "{\n"
-    "\n"
+    "cmd1\n"    // wrong data
+    "}\n"       // wrong data
+    "cmd3\n"    // wrong data
+    "cmd4\n"    // wrong data
+    "{\n"       // wrong data
+    "\n"        // данные закончились/resync
     "{\n"
     "cmd5\n"
     "cmd6\n"
@@ -144,12 +162,12 @@ BOOST_AUTO_TEST_CASE(invalid_bulk_2)
 {
     std::string  in_data = 
     "cmd1\n"
-    "\n"
-    "}\n"
-    "cmd3\n"
-    "cmd4\n"
-    "{\n"
-    "\n"
+    "\n"        // данные закончились/resync
+    "}\n"       // wrong data
+    "cmd3\n"    // wrong data
+    "cmd4\n"    // wrong data
+    "{\n"       // wrong data
+    "\n"        // данные закончились/resync
     "{\n"
     "cmd5\n"
     "cmd6\n"
@@ -168,10 +186,10 @@ BOOST_AUTO_TEST_CASE(invalid_bulk_3)
     std::string  in_data = 
     "cmd1\n" 
     "cmd2\n"
-    "{\n" 
-    "cmd3\n"
-    "cmd4\n" 
-    "\n"
+    "{\n"       // new dinamic bulk
+    "cmd3\n"    // wrong data
+    "cmd4\n"    // wrong data
+    "\n"        // данные закончились/resync
     "cmd5\n"
     "cmd6\n";
 
